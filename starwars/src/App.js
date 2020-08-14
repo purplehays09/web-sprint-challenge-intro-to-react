218560181ca149974a6d66b3ffac42c5eec974c7
purplehays09 <purplehays527@gmail.com>
2020-08-14 15:35:42 -0600

    eh I guess

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,20 +13,52 @@ const App = () => {
   // sync up with, if any.
 
   const [chars, setChars] = useState([])
-  const [currentChar, setCurrentChar] = useState('1')
+  const [charURL, setCharURL] = useState()
 
-  const openDetails = id => {
-    setCurrentChar(id)
+  const openDetails = url => {
+    // let charArr = []
+    // axios.get(url)
+    //   .then(response => {
+    //     console.log(response.data)
+
+    //     CHAR_SPECS.forEach(spec => {
+    //       console.log(spec)
+    //       // console.log(response.data.birth_year)
+    //       console.log(response.data[spec])
+    //       charArr.push(response.data[spec])
+          
+    //     })
+        
+    //   })
+    //   .catch(err => console.log(err))
+    // console.log(charArr)
+    // setCharDetails(charArr)
+
+    // console.log(chars)
+    // chars.map(char => {
+    //   console.log(url)
+    //   if(url === char[url]){
+    //     console.log(char)
+    //     setCharDetails(char)
+    //   }
+    // })
+    setCharURL(url)
+    console.log(charURL)
   }
 
   const closeDetails = () => {
-    setCurrentChar(null)
+    setCharURL(null)
   }
+
+  useEffect(() => {
+
+  })
 
   useEffect(() => {
     axios.get(`${BASE_URL}people/`)
       .then(res => {
         setChars(res.data.results)
+        // console.log(res.data.results)
       })
       .catch(err => {
         console.log(err)
@@ -42,10 +74,9 @@ const App = () => {
         <h1 className="Header">Characters</h1>
       </div>
       <div className='characters'>
-        {chars}
         {
           chars.map(char => {
-            return <p key={char.id}> {char.name}</p>
+            return <Character key={char.url} open={openDetails} close={closeDetails} info={char} url={charURL}> {char.name}</Character>
           })
         }
       </div>
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,15 +1,100 @@
 // Write your Character component here
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {BASE_URL, CHAR_SPECS, CHAR_ARRAYS} from '../constants'
+import styled from 'styled-components'
+import axios from 'axios';
 
 
+const StyledChar = styled.div`
+  width: 60%;
+  margin:0 auto;
 
-export default function Character({info, action,  }) {
+  button {
+    color: ${props => {
+      const { theme } = props;
+      return theme.white;
+    }};
+
+    transition: all 0.5s ease-in-out;
+
+
+  }
+`
+
+export default function Character({info, open, close, url}) {
+    const [details, setDetails] = useState([])
+    const [display, setDisplay] = useState(false)
+    // useEffect(() => {
+    //     // let charArr = []
+    //     // axios.get(url)
+    //     // .then(response => {
+    //     //     console.log(response.data)
+
+    //     //     CHAR_SPECS.forEach(spec => {
+    //     //     console.log(spec)
+    //     //     // console.log(response.data.birth_year)
+    //     //     console.log(response.data[spec])
+    //     //     charArr.push(response.data[spec])
+            
+    //     //     })
+            
+    //     // })
+    //     // .catch(err => console.log(err))
+    //     // console.log(charArr)
+    //     // setDetails(charArr)
+
+    //     // console.log(chars)
+    //     let detailsArr = []
+    //     axios.get(url)
+    //         .then(response => {
+    //             CHAR_SPECS.forEach(spec => {
+    //                 // console.log(spec)
+    //                 // console.log(response.data.birth_year)
+    //                 // console.log(response.data[spec])
+    //                 detailsArr.push(response.data[spec])            
+    //             })
+    //         })
+    //         .catch(err => console.log(err))
+            
+    //     console.log(detailsArr)
+
+    //     setDetails(detailsArr)
+
+    //     console.log(details)
+        
+
+    // },[url])
+
+    function openDeets(){
+
+        setDisplay(!display)
+    }
+
+    useEffect(() => {
+        console.log(display)
+    },[display])
+
     return (
       <div>
         {info.name}
-        <button onClick={() => action(info.id)}>
+        <button onClick={openDeets}>
+            +
         </button>
+
+        <StyledChar style={{display:display ? '' : 'none'}}>
+                {/* {(() => {
+                    for(let i in info){
+                        <p>{info[i]}</p>
+                    }
+                })()} */}
+
+                <p>Birth Year: {info.birth_year}</p>
+                <p>Gender: {info.gender}</p>
+                <p>Eye Color: {info.eye_color}</p>
+                <p>Hair Color: {info.hair_color}</p>
+                <p>Skin Color: {info.skin_color}</p>
+                <p>Height: {info.height}</p>
+        </StyledChar>
       </div>
     )
   }
